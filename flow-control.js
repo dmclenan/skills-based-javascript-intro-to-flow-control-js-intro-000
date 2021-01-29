274e6ccbcf5b893ec0d8f5049b3b6aa244304d1d
Dondre Mclenan <d.mclenan@gmail.com>
2021-01-29 02:05:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,28 +29,17 @@ return "You are not a teenager"
 }
 
 function switchAge(age) {
- switch(age) {
-   case 13:
-    Return "You are a teenager";
-    break
-    case 14:
-    return "You are a teenager";
-    break
-    case 15:
-    return "You are a teenager";
-    break
-    case 16:
-    return "You are a teenager";
-    break
-    case 17:
-    return "You are a teenager";
-    break
-    case 18:
-    return "You are a teenager";
-    break
-    case 19:
-    return "You are a teenager";
-    break
+ switch (age) {
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  case 18:
+  case 19:
+  return "You are a teenager"
+    break;
     default:
-    return "You have an age";}
+    return "You have an age";
+}
 }
